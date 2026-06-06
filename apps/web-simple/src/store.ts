0969e23547858750785361c0c08acddbe7a257cc
brenno luan <brennoluan8530@gmail.com>
2026-06-05 23:16:38 -0300

    feat: add shared store instance to window object for easier access

--- apps/web-simple/src/store.ts
+++ apps/web-simple/src/store.ts
@@ -1,9 +1,15 @@
-import { createSharedStore } from '@repo/store';
-import { useDispatch, useSelector, type TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch } from '@repo/store';
+import { createSharedStore } from "@repo/store";
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from "react-redux";
+import type { RootState, AppDispatch } from "@repo/store";
 
 // Create store instance
 export const store = createSharedStore();
+
+(window as any).__SHARED_STORE__ = store;
 
 // Typed hooks
 export const useAppDispatch = () => useDispatch<AppDispatch>();
